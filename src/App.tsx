1324a9f969535cc33c77505a0b3cf240ad29068a
Joshua Simon <joshy.d.simon@gmail.com>
2022-02-23 11:01:01 +1300

    removed boilerplate, added state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles.css'
+
+
+interface IState {
+  people: {
+    name:string,
+    age:number,
+    url:string,
+    note?:string
+  }[]
+}
 
 function App() {
+
+  const [ people,setPeople ] = useState<IState["people"]>([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>People invited to my party</h1>
     </div>
   );
 }
